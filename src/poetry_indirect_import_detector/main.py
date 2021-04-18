eefc13b6dc547793d8bee5bfd5e7eaf26a36094c
keno <keno.ss57@gmail.com>
2021-04-19 01:01:23 +0900

    refactor

--- src/poetry_indirect_import_detector/main.py
+++ src/poetry_indirect_import_detector/main.py
@@ -1,7 +1,7 @@
 import ast
 import sys
 from pathlib import Path
-from typing import List, Optional
+from typing import List, Optional, Tuple
 
 from .detector import _Detector, _IllegalImportDetected, _read_file
 from .pyproject import _PyProject
@@ -11,15 +11,15 @@ from .util import _flatten, _list_all_python_files
 
 def _process_file(
     module_to_proj: dict[str, str], path: Path  # type: ignore  # reason: dict
-) -> Result[List[_IllegalImportDetected], Exception]:
+) -> Result[Tuple[Path, List[_IllegalImportDetected]], Exception]:
     s = _read_file(path)
     tree = ast.parse(s)
     s_ = s.split("\n")
     detector = _Detector(module_to_proj, path, s_, tree)
-    return Ok(detector.detect())
+    return Ok((path, detector.detect()))
 
 
-def _detect(pyproject: _PyProject, dev: bool) -> Result[List[_IllegalImportDetected], Exception]:
+def _detect(pyproject: _PyProject, dev: bool) -> Result[List[Tuple[Path, List[_IllegalImportDetected]]], Exception]:
     module_to_proj_ = pyproject.load_module_to_proj(dev)
     if module_to_proj_.is_err():
         return Err(module_to_proj_.unwrap_err())
@@ -28,21 +28,14 @@ def _detect(pyproject: _PyProject, dev: bool) -> Result[List[_IllegalImportDetec
     paths = pyproject.target_dirs(dev)
     paths = _flatten([_list_all_python_files(path) for path in paths])
 
-    results = [_process_file(module_to_proj, path) for path in paths]
-    return _merge_results(results)
-
-
-def _merge_results(  # type: ignore  # Missing return statement ...why?
-    results: List[Result[List[_IllegalImportDetected], Exception]]
-) -> Result[List[_IllegalImportDetected], Exception]:
-    if all([result.is_ok() for result in results]):
-        xss = [result.unwrap() for result in results]
-        xs = _flatten(xss)
-        return Ok(xs)
-    else:
-        for result in results:
-            if result.is_err():
-                return result
+    ret = []
+    for path in paths:
+        x = _process_file(module_to_proj, path)
+        if x.is_ok():
+            ret.append(x.unwrap())
+        else:
+            return Err(x.unwrap_err())
+    return Ok(ret)
 
 
 def _main(root: Optional[Path] = None) -> None:
@@ -54,17 +47,18 @@ def _main(root: Optional[Path] = None) -> None:
         raise pyproject_.unwrap_err()
     pyproject = pyproject_.unwrap()
 
-    res_ = _detect(pyproject, False)
+    res = _detect(pyproject, False)
     res_dev = _detect(pyproject, True)
-    res = _merge_results([res_, res_dev])
-
     if res.is_err():
         raise res.unwrap_err()
+    elif res_dev.is_err():
+        raise res_dev.unwrap_err()
     else:
-        xs = res.unwrap()
-        if len(xs) == 0:
+        xs = _flatten([res.unwrap(), res_dev.unwrap()])
+        if all([len(es) == 0 for (_, es) in xs]):
             print("OK")
         else:
-            for x in xs:
-                print(x)
+            for (_, es) in xs:
+                for e in es:
+                    print(e)
             sys.exit(1)
